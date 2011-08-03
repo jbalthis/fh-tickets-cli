a45719f3dd64d59292a1f7dbf231c4b3a4fcb7ea
skalee <skalee@gmail.com>
2011-08-03 19:31:40 +0200

    Redirects to Paypal (hopefully) well.

--- client/default/js/app.js
+++ client/default/js/app.js
@@ -1,4 +1,43 @@
-var checkOutWithPayPal = function() {
-  alert("hahahaha");
+/* 
+ * Translates strings like %2d to their ASCII equivalents (dash in this case)
+ */
+var replaceAsciiHexCodeWithActualCharacter = function(str) {
+  return String.fromCharCode(parseInt(str.substr(1), 16));
 };
+
+/*
+ * Example PayPal response is "TOKEN=EC%2d8F209971RX095473U&TIMESTAMP=2011%2d08%2d03T16%3a38%3a20Z&CORRELATIONID=6befc7bb32e04&ACK=Success&VERSION=63%2e0&BUILD=2020243"
+ * We want to decode it into JavaScript object for more convenient access.
+ *
+ * In order to do so, we:
+ * 1. Split string on &
+ * 2. Split each element on =
+ * 3. Restore escaped ASCII sequences
+ */
+var decodePayPalResponse = function(response) {
+  var decoded = {};
+  response
+    .split('&')
+    .map(function(keyval) {
+      return keyval.split('=');
+    }).forEach(function(pair) {
+      decoded[pair[0]] = pair[1].replace(/%[A-Za-z0-9]{2}/g, replaceAsciiHexCodeWithActualCharacter);
+    });
+  return decoded;
+};
+
+var checkOutWithPayPal = function() {
+  var form = $("#chosenTicketsForm");
+  $
+    .post(form.attr('action'), form.serialize())
+    .success(function(response) {
+      var decoded = decodePayPalResponse(response);
+      //TODO check if ACK=Success
+      var redirectUrl = "https://www.sandbox.paypal.com/uk/cgi-bin/webscr?cmd=_express-checkout-mobile&useraction=commit&token=" + decoded["TOKEN"];
+      window.location = redirectUrl;
+    });
+};
+
+$(function() {
+});
 
