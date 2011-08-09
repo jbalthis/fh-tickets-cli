9ee72f93d0fea7af96d893c21fbb6f683bbd0a07
skalee <skalee@gmail.com>
2011-08-09 14:08:00 +0200

    Some syntax fixes.

--- cloud/main.js
+++ cloud/main.js
@@ -1,15 +1,15 @@
 function ppSetPayment(ticketsVIP, ticketsA, ticketsB) {
   return "OK SO FAR";
   var requestParams = [
-      {name:'VERSION', value:'63.0'},
-      {name:'USER', value="skalee_1312461335_biz_api1.gmail.com"},
-      {name:'PWD', value="1312461375"},
-      {name:'SIGNATURE', value="AFcWxV21C7fd0v3bYYYRCpSSRl31A3a7vMmHXJAJHHhlsK-5OAyyuu9b"},
-      {name:'RETURNURL', value="http://www.YourReturnURL.com"},
-      {name:'CANCELURL', value="http://www.YourCancelURL.com"},
-      {name:'METHOD', value="SetExpressCheckout"},
-      {name:'PAYMENTREQUEST_0_CURRENCYCODE', value="EUR"},
-      {name:'PAYMENTREQUEST_0_AMT', value="0"}
+    {name: 'VERSION', value: '63.0'},
+    {name: 'USER', value: "skalee_1312461335_biz_api1.gmail.com"},
+    {name: 'PWD', value: "1312461375"},
+    {name: 'SIGNATURE', value: "AFcWxV21C7fd0v3bYYYRCpSSRl31A3a7vMmHXJAJHHhlsK-5OAyyuu9b"},
+    {name: 'RETURNURL', value: "http://www.YourReturnURL.com"},
+    {name: 'CANCELURL', value: "http://www.YourCancelURL.com"},
+    {name: 'METHOD', value: "SetExpressCheckout"},
+    {name: 'PAYMENTREQUEST_0_CURRENCYCODE', value: "EUR"},
+    {name: 'PAYMENTREQUEST_0_AMT', value: "0"}
   ];
 
   var ticketsAndPrices = [[ticketsVIP, 300, "VIP Sector tickets"], [ticketsA, 30, "Sector A tickets"], [ticketsB, 10, "Sector B tickets"]];
