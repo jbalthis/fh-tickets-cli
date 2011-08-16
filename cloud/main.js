e32b9cb0443957fd521249964616819afc3d097d
skalee <skalee@gmail.com>
2011-08-16 13:09:27 +0200

    PayPal.

--- cloud/main.js
+++ cloud/main.js
@@ -68,7 +68,7 @@ function trySettingUpTransaction(triesLeft) {
     ],
     cookies: [
     ],
-    period: 1360000
+    period: 0//1360000
   });
 
   $fh.log('debug', 'tmp ' + $fh.stringify(response));
