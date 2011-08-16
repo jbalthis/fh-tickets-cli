e858c2a7c874af795f2c97a3999c136d91846d78
skalee <skalee@gmail.com>
2011-08-16 10:37:56 +0200

    PP

--- cloud/main.js
+++ cloud/main.js
@@ -1,3 +1,33 @@
+/*
+ * Translates strings like %2d to their ASCII equivalents (dash in this case)
+ */
+var replaceAsciiHexCodeWithActualCharacter = function (str) {
+  return String.fromCharCode(parseInt(str.substr(1), 16));
+};
+
+/*
+ * Example PayPal response is "TOKEN=EC%2d8F209971RX095473U&TIMESTAMP=2011%2d08%2d03T16%3a38%3a20Z&CORRELATIONID=6befc7bb32e04&ACK=Success&VERSION=63%2e0&BUILD=2020243"
+ * We want to decode it into JavaScript object for more convenient access.
+ *
+ * In order to do so, we:
+ * 1. Split string on &
+ * 2. Split each element on =
+ * 3. Restore escaped ASCII sequences
+ */
+var decodePayPalResponse = function (response) {
+  var decoded = {};
+  response
+    .split('&')
+    .map(function (keyval) {
+      return keyval.split('=');
+    }).forEach(function (pair) {
+      decoded[pair[0]] = pair[1].replace(/%[A-Za-z0-9]{2}/g, replaceAsciiHexCodeWithActualCharacter);
+    });
+  return decoded;
+};
+
+/*---*/
+
 function pFetchConfig() {
   var configuration = [
     {name: 'VERSION', value: '63.0'},
@@ -13,14 +43,16 @@ function pFetchConfig() {
   return {config: configuration};
 }
 
-function pSetPayment() {
+function trySettingUpTransaction(triesLeft) {
+  if (triesLeft === 0) return false;
+
   var requestParams = [
     {name: 'VERSION', value: '63.0'},
     {name: 'USER', value: "skalee_1312461335_biz_api1.gmail.com"},
     {name: 'PWD', value: "1312461375"},
     {name: 'SIGNATURE', value: "AFcWxV21C7fd0v3bYYYRCpSSRl31A3a7vMmHXJAJHHhlsK-5OAyyuu9b"},
-    {name: 'RETURNURL', value: "http://www.YourReturnURL.com"},
-    {name: 'CANCELURL', value: "http://www.YourCancelURL.com"},
+    {name: 'RETURNURL', value: $fh.util({'cloudUrl': 'pUserAccepts'}).cloudUrl},
+    {name: 'CANCELURL', value: $fh.util({'cloudUrl': 'pUserDenies'}).cloudUrl},
     {name: 'METHOD', value: "SetExpressCheckout"},
     {name: 'PAYMENTREQUEST_0_CURRENCYCODE', value: "EUR"},
     {name: 'PAYMENTREQUEST_0_AMT', value: "100"}
@@ -39,7 +71,16 @@ function pSetPayment() {
     period: 1360000
   });
 
-  return response;
+  return (response.body ? response : trySettingUpTransaction(triesLeft - 1);
+}
+
+function pSetPayment() {
+  var response = trySettingUpTransaction(7);
+
+  if (!response) return({'error': 'error'});
+
+  $fh.log('debug', response.body);
+  return({'response': decodePayPalResponse(response)});
 }
 
 function oldPayment() {
@@ -62,6 +103,7 @@ function oldPayment() {
 
 function pUserAccepts() {
   $fh.log('info', 'User decides to pay');
+
   return {};
 }
 
