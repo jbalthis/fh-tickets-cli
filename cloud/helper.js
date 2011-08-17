e13f09b5c031bf607a3c72d60fc53509137b1e78
skalee <skalee@gmail.com>
2011-08-17 13:36:03 +0200

    PayPal.

---
+++ cloud/helper.js
@@ -0,0 +1,25 @@
+var trySettingUpTransaction = function (triesLeft) {
+  if (triesLeft === 0) return false;
+
+  var requestParams = [
+    {name: 'RETURNURL', value: $fh.util({'cloudUrl': 'pUserAccepts'}).cloudUrl},
+    {name: 'CANCELURL', value: $fh.util({'cloudUrl': 'pUserDenies'}).cloudUrl},
+    {name: 'METHOD', value: "SetExpressCheckout"}
+  ].concat(prepareTransactionDetails );
+
+  var response = $fh.web({
+    url: "https://api-3t.sandbox.paypal.com/nvp",
+    method: 'POST',
+    charset: 'UTF-8',
+    contentType: 'text/plain',
+    params: makeApiParams(requestParams),
+    headers: [
+    ],
+    cookies: [
+    ],
+    period: 4000
+  });
+
+  return (response.body ? response : trySettingUpTransaction(triesLeft - 1));
+};
+
---
+++ cloud/paypal_params.js
@@ -0,0 +1,52 @@
+/*
+ * Example PayPal response is "TOKEN=EC%2d8F209971RX095473U&TIMESTAMP=2011%2d08%2d03T16%3a38%3a20Z&CORRELATIONID=6befc7bb32e04&ACK=Success&VERSION=63%2e0&BUILD=2020243"
+ * We want to decode it into JavaScript object for more convenient access.
+ *
+ * In order to do so, we:
+ * 1. Split string on &
+ * 2. Split each element on =
+ * 3. Restore escaped ASCII sequences
+ */
+var decodePayPalResponse = function (response) {
+  var decoded = {};
+  response
+    .split('&')
+    .map(function (keyval) {
+      return keyval.split('=');
+    }).forEach(function (pair) {
+      decoded[pair[0]] = pair[1].replace(/%[A-Za-z0-9]{2}/g, replaceAsciiHexCodeWithActualCharacter);
+    });
+  return decoded;
+};
+
+
+
+/*
+ * Translates strings like %2d to their ASCII equivalents (dash in this case)
+ */
+var replaceAsciiHexCodeWithActualCharacter = function (str) {
+  return String.fromCharCode(parseInt(str.substr(1), 16));
+};
+
+
+
+/*
+ * Extends passed params for PayPal operations with credentials
+ */
+var makeApiParams = function (params) {
+  var authParams = [
+    {name: 'VERSION', value: '63.0'},
+    {name: 'USER', value: "skalee_1312461335_biz_api1.gmail.com"},
+    {name: 'PWD', value: "1312461375"},
+    {name: 'SIGNATURE', value: "AFcWxV21C7fd0v3bYYYRCpSSRl31A3a7vMmHXJAJHHhlsK-5OAyyuu9b"},
+  ];
+  return params.concat(authParams);
+};
+
+var prepareTransactionDetails = function () {
+  return [
+    {name: 'PAYMENTREQUEST_0_CURRENCYCODE', value: "EUR"},
+    {name: 'PAYMENTREQUEST_0_AMT', value: "10"}
+  ];
+};
+
