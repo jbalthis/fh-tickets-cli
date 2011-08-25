7c8d895ce2ee884b72b9ca9cb955f016a3843b28
skalee <skalee@gmail.com>
2011-08-25 12:01:51 +0200

    PayPal.

--- cloud/paypal.js
+++ cloud/paypal.js
@@ -75,6 +75,7 @@ var tryCommunicatingWithPayPal = function (params, triesLeft) {
     period: 4000
   });
 
+  $fh.log('debug', '%%%%' + $fh.serialize(response));
   if (response.body) {
     return decodePayPalResponse(response.body);
   } else {
