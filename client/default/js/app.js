32e65d73842189a2ed8097c9db25a116e3436484
skalee <skalee@gmail.com>
2011-09-06 13:20:56 +0200

    Paypal.

--- client/default/js/app.js
+++ client/default/js/app.js
@@ -1,14 +1,35 @@
-var communicateTillSuccess = function(act, req) {
+var paymentFunctions = {
+  setUp: function() {
+    var setUpResponse = communicateTillSuccess('pSetPayment', {
+      ticketsVIP: $('input[name=VIP]').val(),
+      ticketsA:   $('input[name=SectorA]').val(),
+      ticketsB:   $('input[name=SectorB]').val()
+    }, onSetUp);
+  },
+  onSetUp: function(response) {
+    //$fh.webview({'url': response.redirectUrl, 'title': null});
+    window.location = response.redirectUrl;
+  },
+  onRetrieveDetails: function(response) {
+    var retrieveResponse = communicateTillSuccess('pRetrievePayerDetails', {token: setUpResponse.token});
+    var finalizeResponse = communicateTillSuccess('pFinalizePayment', {token: setUpResponse.token});
+  alert ('done.');
+  },
+  onFinalize: function(response) {
+  }
+};
+
+var communicateTillSuccess = function(act, req, nextStep) {
   $fh.act({
     act: 'pSetPayment',
     req: req
-  }, function(response) {
+  }, /*jsl:ignore*/function(response) {
     if (response.status && response.status === 'ok') {
-      return response;
+      return nextStep(response); //Not very necessary, but
     } else {
       return communicateTillSuccess(act, req);
     }
-  });
+  }/*jsl:end*/);
 };
 
 var checkOutWithPayPal = function () {
@@ -17,18 +38,7 @@ var checkOutWithPayPal = function () {
     return false;
   }
 
-  var setUpResponse = communicateTillSuccess('pSetPayment', {
-    ticketsVIP: $('input[name=VIP]').val(),
-    ticketsA:   $('input[name=SectorA]').val(),
-    ticketsB:   $('input[name=SectorB]').val()
-  });
-
-  //$fh.webview({'url': response.redirectUrl, 'title': null});
-  window.location = setUpResponse.redirectUrl;
-
-  var retrieveResponse = communicateTillSuccess('pRetrievePayerDetails', {token: setUpResponse.token});
-  var finalizeResponse = communicateTillSuccess('pFinalizePayment', {token: setUpResponse.token});
-  alert ('done.');
+  paymentFunctions.setUp();
   return false;
 };
 
